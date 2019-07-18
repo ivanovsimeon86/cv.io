fc0b0dab4b758227952a64564ab8ed7464d0a43d
Simeon Ivanov <simeon.ivanov@zautre.com>
2019-07-18 17:04:23 +0300

    have fun

--- app.js
+++ app.js
@@ -1,28 +1,35 @@
-// 1. Define route components.
-// These can be imported from other files
-const Foo = { template: '<div>foo</div>' }
-const Bar = { template: '<div>bar</div>' }
-
-// 2. Define some routes
-// Each route should map to a component. The "component" can
-// either be an actual component constructor created via
-// `Vue.extend()`, or just a component options object.
-// We'll talk about nested routes later.
-const routes = [
-  { path: '/foo', component: Foo },
-  { path: '/bar', component: Bar }
-]
-
-// 3. Create the router instance and pass the `routes` option
-// You can pass in additional options here, but let's
-// keep it simple for now.
-const router = new VueRouter({
-  routes // short for `routes: routes`
-})
-
-// 4. Create and mount the root instance.
-// Make sure to inject the router with the router option to make the
-// whole app router-aware.
-const app = new Vue({
-  router
-}).$mount('#app')
+$(document).ready(function(){
+// Swiching content functionality
+    $('#home').on('click', function(){
+        $('.active').removeClass('active');
+        $(this).addClass('active')
+        $('.visible').removeClass('visible');
+        $('.home').addClass('visible')
+    });
+    $('#about').on('click', function(){
+        $('.active').removeClass('active');
+        $(this).addClass('active')
+        $('.visible').removeClass('visible');
+        $('.about').addClass('visible')
+    });
+    $('#contacts').on('click', function(){
+        $('.active').removeClass('active');
+        $(this).addClass('active')
+        $('.visible').removeClass('visible');
+        $('.contacts').addClass('visible')
+    });
+    /* writeText(); */
+});
+//Text writing function
+/* function writeText () {
+    let text = 'some text';
+    let element = $('.home');
+    let i = 0;
+    while (i < text.length) {
+        setInterval(() => {
+        let cuttedText = text.slice(0,i);
+        element.val(cuttedText);
+            i++
+        }, 500);
+    }
+} */
